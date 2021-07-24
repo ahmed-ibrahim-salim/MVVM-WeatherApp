28f026dbe5d3d073b7816689c4f159d76e369cbc
medobebo <ahmed.ibrahim2090@yahoo.com>
2021-07-24 09:58:27 +0200

    Initial Commit

---
+++ MVVM-WeatherApp/MVVM_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_WeatherAppApp.swift
+//  MVVM-WeatherApp
+//
+//  Created by ahmadmedo on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
